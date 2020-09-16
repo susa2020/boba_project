9e9c46210f1f1188ef04092fcd707cc6ef2bc4aa
viere1234 <69096696+viere1234@users.noreply.github.com>
2020-09-16 23:26:27 +0800

    BrianChang

--- app/src/main/java/com/example/susa_boba_project/MainActivity.java
+++ app/src/main/java/com/example/susa_boba_project/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.susa_boba_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void add_bubble_clicked(View view) {
+        Toast toast = Toast.makeText(this, "新增按鈕已經被點擊", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+    public void edit_button_clicked(View view) {
+        Toast toast = Toast.makeText(this, "編輯按鈕已經被點擊", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+    public void more_clicked(View view) {
+        Toast toast = Toast.makeText(this, "更多按鈕已經被點擊", Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
