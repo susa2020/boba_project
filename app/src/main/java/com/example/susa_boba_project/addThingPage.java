01a438c62b878a954bf2ba440f8611732607df2d
qingwei4 <62367550+qingwei4@users.noreply.github.com>
2020-10-28 22:57:44 +0800

    新增頁面(僅完成部分)

---
+++ app/src/main/java/com/example/susa_boba_project/addThingPage.java
@@ -0,0 +1,54 @@
+package com.example.susa_boba_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Canvas;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.content.Intent;
+
+public class addThingPage extends AppCompatActivity {
+    private Button memo_added_button;
+    private EditText type_memo;
+    public Integer cnt=0;
+    SharedPreferences thingsToDo = getSharedPreferences("toDoList", MODE_PRIVATE);
+    protected void onCreate( Bundle saveInstanceState ){
+        super.onCreate( saveInstanceState );
+        setContentView( R.layout.pop_up_memo_added );
+        type_memo = ( EditText )findViewById( R.id.type_memo );
+        memo_added_button = ( Button )findViewById( R.id.memo_added_button );
+        memo_added_button.setOnClickListener( addThingToTheList );
+    }
+    private Button.OnClickListener addThingToTheList =
+            new Button.OnClickListener(){
+            @Override
+                public void Onclick( View v ){
+                    public String thingThatAddedNow = type_memo.getText().toString();
+                    if( thingThatAddedNow.length() == 0 ){
+                        Toast nothingHasInputed = Toast.makeText( addThingPage.this , "輸入為空!" , Toast.LENGTH_SHORT );
+                        nothingHasInputed.show();
+                    }
+                    else{
+                        thingsToDo.edit()
+                                .putString( cnt , thingThatAddedNow )
+                                .commit();
+                        Toast thingHasBeenAdded = Toast.makeText( addThingPage.this , "您輸入的事項已被新增!" , Toast.LENGTH_SHORT );
+                        thingHasBeenAdded.show();
+                        Intent intent = new Intent();
+                        intent.setClass( addThingPage.this , MainActivity.class );
+                        startActivity( intent );
+                    }
+                }
+            };
+}
