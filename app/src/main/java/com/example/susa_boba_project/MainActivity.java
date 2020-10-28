417d2b5cbddc72da5c5022904c814a66b03fd18e
qingwei4 <62367550+qingwei4@users.noreply.github.com>
2020-10-28 22:52:39 +0800

    新增頁面(僅完成部分)

--- app/src/main/java/com/example/susa_boba_project/MainActivity.java
+++ app/src/main/java/com/example/susa_boba_project/MainActivity.java
@@ -1,29 +1,36 @@
 package com.example.susa_boba_project;
 
-import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import android.content.Intent;
 
 import com.google.android.material.textfield.TextInputEditText;
 
-import java.util.ArrayList;
-
 public class MainActivity extends AppCompatActivity {
+
     //進度:資料庫架好功能尚未完成沒意外會報錯
    private final static String _ID = "_id";
    private final static String MEMO_CONTENT = "memo_content";
-   //private LastView lstView;
-   private Button btnappend,btnUpdate,btnSelect;
+   private LastView lstView;
+   private Button btnappend,btnUpdate,btnSelect,addBuddleButton;
    private TextInputEditText edtId,edtContent;
    private MemoBubble db = new MemoBubble(MainActivity.this);
    private Cursor cursor;
+   boolean addBuddleButtonHasBeenClicked = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,32 +39,28 @@ public class MainActivity extends AppCompatActivity {
         db = new MemoBubble(MainActivity.this);
         db.open();
         cursor = db.select_all();
-       // UpdateListView(cursor);
-
-        //recrclerview start
-        setContentView(R.layout.activity_main);
-
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.bubbles_recyclerView);
-
-        ArrayList<Memo> data = new ArrayList<>();
-        data.add(new Memo("Marshmallow"));//test
-        data.add(new Memo("Lollipop"));//test
-
-        bubbles_recyclerview_adapter adapter = new bubbles_recyclerview_adapter(this, data);
-
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(adapter);
-        //recyclerview end
+        UpdateListView(cursor);
+        addBuddleButton = ( Botton )findViewById( R.id.addBubbleButton );
     }
-
+    private addBuddleButton.OnClickListener addThingToList =
+            new addBuddleButton.OnclickListener(){
+            @Override
+                public void onClick( View  v ){
+                    addBuddleButtonHasBeenClicked = true;
+                    Intent intent = new intent;
+                    intent.setClass( MainActivity.this , addThingPage.class );
+                    String showedThing;
+                    startActivity( intent );
+                }
+            }
     private void findViews(){
-        //建立lisener
-        //類別偵聽氣
+        //建立listener
+        //類別偵聽器
         //onItemLongClick未完成
         //onClicker未完成
     }
-   /* public void UpdateListView(Cursor memocursor){
-        bubbles_recyclerview_adapter adapter = new bubbles_recyclerview_adapter(memocursor);
+    public void UpdateListView(Cursor memocursor){
+        MyAdapter adapter = new MyAdapter(memocursor);
         adapter.notifyDataSetChanged();
         lstView.setAdapter(adapter);
         cursor = memocursor;
@@ -90,11 +93,14 @@ public class MainActivity extends AppCompatActivity {
             return getview;
         }
 
-    }*/
+    }
+
+    @Override
+
+
     public void add_bubble_clicked(View view) {
         Toast toast = Toast.makeText(this, "新增按鈕已經被點擊", Toast.LENGTH_SHORT);
         toast.show();
-        db.append(new Memo("add"));//test
 
     }
     public void edit_button_clicked(View view) {
@@ -104,9 +110,6 @@ public class MainActivity extends AppCompatActivity {
     public void more_clicked(View view) {
         Toast toast = Toast.makeText(this, "更多按鈕已經被點擊", Toast.LENGTH_SHORT);
         toast.show();
-        Intent it = new Intent(this, SettingsActivity.class); //creat intent and set the activity
-        startActivity(it); // go to second activity
-
     }
 
 
